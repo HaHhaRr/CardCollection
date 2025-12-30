c923c054e153cccccfd253eee9240f9be9cf7291
Zahar <HaHhaRr>
2025-12-30 20:58:02 +0300

    ci/cd

--- src/test/java/ru/hahharr/cardcollection/Testing.java
+++ src/test/java/ru/hahharr/cardcollection/Testing.java
@@ -1,20 +1,10 @@
 package ru.hahharr.cardcollection;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.hahharr.cardcollection.models.orm.DropChanceOrm;
-import ru.hahharr.cardcollection.models.orm.PackOrm;
-
-import java.io.IOException;
 
 public class Testing {
 
     @Test
-    public void usHundred() throws IOException {
-        DropChanceOrm dropChanceOrm = DropChanceOrm.createFromChances(50,
-                40,
-                10,
-                new PackOrm());
-        Assertions.assertEquals(0.5, dropChanceOrm.getCommonDropChance());
+    public void test() {
     }
 }
