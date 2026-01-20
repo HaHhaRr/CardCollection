c92b4e78384885656814cf07cc340505e384296a
Zahar <HaHhaRr>
2026-01-20 13:17:15 +0300

    added sort userCollection by rarity and collection resolve conversation #2

--- src/main/java/ru/hahharr/cardcollection/utils/resolvers/PageableResolver.java
+++ src/main/java/ru/hahharr/cardcollection/utils/resolvers/PageableResolver.java
@@ -8,22 +8,21 @@ import java.util.Optional;
 
 public class PageableResolver {
 
-    private static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "Wrong sortBy value";
-
     public static Pageable resolveSortBy(int offset, int limit, String sortBy, String direction) {
         if (sortBy == null) {
             return OffsetLimitPage.of(offset, limit);
         }
+
         Optional<Sort> sort = switch (sortBy) {
             case "rarity" -> Optional.of(Sort.by(sortBy));
             case "collection" -> Optional.of(Sort.by("collectionOrm.id"));
-            default -> throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE);
+            default -> Optional.empty();
         };
 
         if ("descending".equals(direction)) {
             sort = sort.map(Sort::descending);
         }
 
-        return OffsetLimitPage.of(offset, limit, sort.get());
+        return OffsetLimitPage.of(offset, limit, sort.orElse(Sort.unsorted()));
     }
 }
